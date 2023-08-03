29f412e5081e3be20a4f5dd60f3e4f891c3efd96
Riejan <riejanevangelista1620@gmail.com>
2023-08-03 16:30:17 +0800

    Update Portfolio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -39,12 +39,18 @@ export const About = () => {
     <section  id="about" className="bg-white py-20 flex items-center justify-center overflow-hidden ">
       <motion.div  initial="offScreen" whileInView="onScreen" viewport={{ once: true}}  className="m-auto sm:w-[90%] md:w-[60%] grid xl:grid-cols-2 gap-10">
         <motion.div variants={imageVariant}  className="flex justify-center items-center">
-          <div className="w-full relative ">
+          <div className="relative z-50">
             <img
-              className="w-full h-full rounded-xl h-full shadow-mmd border-t border-l border-slate-600 "
-              src="/images/about.jpeg"
+              className="w-full h-full rounded-xl h-full z-50 "
+              src="/images/about.png"
               alt=""
             />
+            <div className="rounded-full bg-blue-400 absolute w-[60px] h-[60px] top-0 z-0"></div>
+            <div className="rounded-full bg-emerald-400 absolute w-[60px] h-[60px] top-0 right-0 z-0"></div>
+            <div className="rounded-full bg-red-400 absolute w-[60px] h-[60px] bottom-0 right-0 z-0"></div>
+            <div className="rounded-full bg-indigo-400 absolute w-[60px] h-[60px] bottom-0 left-0 z-0"></div>
+
+            
           </div>
         </motion.div>
         <motion.div variants={textVariant} className="flex flex-col gap-5 sm:text-center xl:text-justify">
