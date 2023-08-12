152d89f689c5733a71b843d48084cd66b994fb10
Riejan <riejanevangelista1620@gmail.com>
2023-08-12 19:23:00 +0800

    Update Portfolio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,44 +1,51 @@
 import React from "react";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 
 export const About = () => {
-
   const imageVariant = {
     offScreen: {
-      y:300,
+      y: 300,
       opacity: 0,
     },
     onScreen: {
-      y:5,
-      opacity:1,
+      y: 5,
+      opacity: 1,
       transition: {
-        type:"spring",
-        bounce:0.4,
-        duration:1,
-      }
-    }
-  }
+        type: "spring",
+        bounce: 0.4,
+        duration: 1,
+      },
+    },
+  };
 
   const textVariant = {
-    offScreen:{
-      x:100,
-      opacity:0,
+    offScreen: {
+      x: 100,
+      opacity: 0,
     },
     onScreen: {
-      x:5,
-      opacity:1,
+      x: 5,
+      opacity: 1,
       transition: {
-        type:"spring",
-        bounce:0.4,
-        duration:1,
-      }
-    }
-  }
- 
+        type: "spring",
+        bounce: 0.4,
+        duration: 1,
+      },
+    },
+  };
+
   return (
-    <section  id="about" className="bg-white py-20 flex items-center justify-center overflow-hidden ">
-      <motion.div  initial="offScreen" whileInView="onScreen" viewport={{ once: true}}  className="m-auto sm:w-[90%] md:w-[60%] grid xl:grid-cols-2 gap-10">
-        <motion.div variants={imageVariant}  className="flex justify-center items-center">
+    <section
+      id="about"
+      className="bg-white py-20 flex items-center justify-center overflow-hidden ">
+      <motion.div
+        initial="offScreen"
+        whileInView="onScreen"
+        viewport={{ once: true }}
+        className="m-auto sm:w-[90%] md:w-[60%] grid xl:grid-cols-2 gap-10">
+        <motion.div
+          variants={imageVariant}
+          className="flex justify-center items-center">
           <div className="relative z-50">
             <img
               className="w-full h-full rounded-xl h-full z-50 "
@@ -49,22 +56,25 @@ export const About = () => {
             <div className="rounded-full bg-emerald-400 absolute w-[60px] h-[60px] top-0 right-0 z-0"></div>
             <div className="rounded-full bg-red-400 absolute w-[60px] h-[60px] bottom-0 right-0 z-0"></div>
             <div className="rounded-full bg-indigo-400 absolute w-[60px] h-[60px] bottom-0 left-0 z-0"></div>
-
-            
           </div>
         </motion.div>
-        <motion.div variants={textVariant} className="flex flex-col gap-5 sm:text-center xl:text-justify">
+        <motion.div variants={textVariant} className="flex flex-col space-y-5 space-x-2 ">
           <h1 className="text-blue-400 font-bold">ABOUT ME 😉</h1>
           <h2 className="text-2xl text-slate-900 font-bold sm:text-center md:text-start">
-            A dedicated Front-end Developer based in Rizal, Philippines 📍
+            Aspiring Web Developer
           </h2>
-          <motion.p initial="offScreen" whileInView="onScreen" viewport={{ once: true, amount: 0.8 }} className=" opacity-60">
-            As a Front-End Developer, I possess an impressive arsenal of
-            skills in HTML, CSS, JavaScript, React, Tailwind, DaisyUI and Figma for Web Design Prototype. I excel
-            in designing and maintaining responsive websites that offer a smooth
-            user experience. My expertise lies in crafting dynamic, engaging
-            interfaces through writing clean and optimized code and utilizing
-            cutting-edge development tools and techniques. 
+          <motion.p
+            initial="offScreen"
+            whileInView="onScreen"
+            viewport={{ once: true, amount: 0.8 }}
+            className="text-gray-500 tracking-wide space-y-2 leading-8">
+            As a dedicated and passionate learner in the field of development I
+            have an interest in creating interactive and user focused web
+            applications. I have built my foundation learning the
+            fundamentals.HTML, CSS and JavaScript to build visually appealing
+            and responsive front end interfaces. I am enthusiastic, about
+            contributing my skills to design user experiences while working
+            collaboratively within a team oriented setting.
           </motion.p>
         </motion.div>
       </motion.div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,58 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-
 export const Home = () => {
-
   const titleVariant = {
     offScreen: {
       opacity: 0,
-      x:-50,
+      x: -50,
     },
     onScreen: {
-      opacity:1,
-      x:0,
-      transition:{
-        type:'spring',
-        bounce:0.5,
-        duration:0.8
-      }
-    }
-  }
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.5,
+        duration: 0.8,
+      },
+    },
+  };
 
   const textVariant = {
     offScreen: {
       opacity: 0,
-      x:-50,
+      x: -50,
     },
     onScreen: {
-      opacity:1,
-      x:0,
-      transition:{
-        type:'spring',
-        bounce:0.5,
-        duration:1
-      }
-    }
-  }
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.5,
+        duration: 1,
+      },
+    },
+  };
 
   return (
-    <section id="home" className="xl:h-screen sm:py-20 bg-slate-50 flex items-center justify-center">
+    <section
+      id="home"
+      className="relative overflow-hidden xl:h-screen sm:py-20 bg-slate-50 flex items-center justify-center">
       <div className="m-auto sm:w-[90%] md:w-[60%] flex sm:flex-col-reverse xl:flex-row gap-5">
         <motion.div>
-          <motion.h1 variants={titleVariant} initial="offScreen" whileInView={`onScreen`} className="relative  xl:text-5xl sm:text-[30px] md:text-6xl  sm:text-center xl:text-start tracking-wide font-bold text-gray-800">
-           Front-End Developer <span className="absolute animate-wiggle">👋🏻</span>
+          <motion.h1
+            variants={titleVariant}
+            initial="offScreen"
+            whileInView={`onScreen`}
+            className="relative  xl:text-5xl sm:text-[30px] md:text-6xl  sm:text-center xl:text-start tracking-wide font-bold text-gray-800">
+            Front-End Developer{" "}
+            <span className="absolute animate-wiggle">👋🏻</span>
           </motion.h1>
-          <motion.p variants={textVariant} initial="offScreen" whileInView={`onScreen`} className="sm:text-center xl:text-start text-gray-800 tracking-wide text-[16px] sm:mt-0 md:mt-10 leading-6">
+          <motion.p
+            variants={textVariant}
+            initial="offScreen"
+            whileInView={`onScreen`}
+            className="sm:text-center xl:text-start text-gray-800 tracking-wide text-[16px] sm:mt-0 md:mt-10 leading-6">
             Hi, I'm Riejan Evangelista, Aspiring to be a Front End Developer
             based in Rizal, Philippines 📍
           </motion.p>
@@ -58,32 +67,86 @@ export const Home = () => {
               <i className="fa-brands fa-square-github text-3xl ml-5"></i>
             </a>
           </div>
-          <div className="teck-stack  flex sm:flex-col xl:flex-row items-center justify-between gap-2 xl:mt-0 sm:mt-10 ">
-                <p className="border-r border-slate-900 pr-5 text-slate-900 font-semibold">Tech Stack</p>
-                <div className="tech-icons  transition grid sm:grid-cols-3 content-center justify-content-center xl:grid-cols-6 gap-5 xl:mt-0 sm:mt-0">
-                  <motion.div initial={{opacity:0, y:-50}} whileInView={{opacity:1, y:0}} transition={{duration:0.7, type:'spring', bounce:0.4}} className="w-[60px] bg-white shadow-xl rounded-full  p-4 cursor-pointer "><img className="w-full h-full object-cover" src="images/html.png" alt="" /></motion.div>  
-                  <motion.div initial={{opacity:0, y:-50}} whileInView={{opacity:1, y:0}} transition={{duration:0.9, type:'spring', bounce:0.4}}  className="w-[60px] bg-white shadow-xl rounded-full  p-4 cursor-pointer "><img className="w-full h-full object-cover" src="images/css.png" alt="" /></motion.div>  
-                  <motion.div initial={{opacity:0, y:-50}} whileInView={{opacity:1, y:0}} transition={{duration:1.1, type:'spring', bounce:0.4}}  className="w-[60px] bg-white shadow-xl rounded-full  p-4 cursor-pointer "><img className="w-full h-full object-cover" src="images/js.png" alt="" /></motion.div>  
-                  <motion.div initial={{opacity:0, y:-50}} whileInView={{opacity:1, y:0}} transition={{duration:1.3, type:'spring', bounce:0.4}}  className="w-[60px] bg-white shadow-xl rounded-full  p-4 cursor-pointer "><img className="w-full h-full object-cover" src="images/react.png" alt="" /></motion.div>  
-                  <motion.div initial={{opacity:0, y:-50}} whileInView={{opacity:1, y:0}} transition={{duration:1.5, type:'spring', bounce:0.4}} className="w-[60px] bg-white shadow-xl rounded-full  p-4 cursor-pointer "><img className="w-full h-full object-cover" src="images/tailwind.png" alt="" /></motion.div>  
-                </div>
+          <div className="relative flex sm:flex-col xl:flex-row items-center justify-around xl:mt-0 sm:mt-10 z-40 ">
+            <p className="border-r border-slate-900 pr-5 text-slate-900 font-semibold">
+              Tech Stack
+            </p>
+            <div className=" tech-icons  transition flex flex-wrap justify-center gap-5 xl:mt-0 sm:mt-5">
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7, type: "spring", bounce: 0.4 }}
+                className="  w-[60px] bg-white shadow-xl rounded-full  p-2    ">
+                <img
+                  className="w-full h-full object-cover rounded-full"
+                  src="images/html.svg"
+                  alt=""
+                />
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1.1, type: "spring", bounce: 0.4 }}
+                className=" w-[60px] bg-white shadow-xl rounded-full  p-2   ">
+                <img
+                  className="w-full h-full object-cover"
+                  src="images/css.svg"
+                  alt=""
+                />
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1.6, type: "spring", bounce: 0.4 }}
+                className=" w-[60px]      bg-white shadow-xl rounded-full  p-2   ">
+                <img
+                  className="w-full h-full object-cover rounded-full"
+                  src="images/javascript.svg"
+                  alt=""
+                />
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2.1, type: "spring", bounce: 0.4 }}
+                className=" w-[60px]    bg-white shadow-xl rounded-full  p-2  ">
+                <img
+                  className="w-full h-full object-cover"
+                  src="images/react.svg"
+                  alt=""
+                />
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2.7, type: "spring", bounce: 0.4 }}
+                className="  w-[60px] bg-white shadow-xl rounded-full  p-2   ">
+                <img
+                  className="w-full h-full object-cover"
+                  src="images/tailwind.svg"
+                  alt=""
+                />
+              </motion.div>
+            </div>
           </div>
-          
         </motion.div>
         <div className="flex items-center justify-center">
-          <motion.div initial={{opacity:0, x:50}} whileInView={{opacity:1, x:0}} transition={{duration:0.6,}}  className="rounded-full sm:w-[300px] xl:w-[300px]  relative">
-            
+          <motion.div
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            className="rounded-full sm:w-[300px] xl:w-[300px]  relative">
             <img
               className="animate-blob w-full border border-slate-100 z-50 h-full object-cover"
               src="/profile.jpg"
               alt=""
             />
-            
           </motion.div>
         </div>
-        
       </div>
-      
+
+      <motion.div initial={{ scale:0.1}} whileInView={{scale:1}} transition={{duration:2, type: "spring", bounce: 0.1, }} viewport={{once:true}} className="w-[600px] h-[600px] rounded-full bg-blue-300 absolute bottom-[-320px] left-[-320px]"></motion.div>
+      <motion.div initial={{scale:0.1}} whileInView={{scale:1}} transition={{duration:2, type: "spring", bounce: 0.1, }} viewport={{once:true}} className="w-[600px] h-[600px] rounded-full bg-green-300 absolute bottom-[-320px] right-[-320px]"></motion.div>
     </section>
   );
 };
